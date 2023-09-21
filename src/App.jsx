eeb373e7d57d6bdd6f69edee286c5f80e0137012
b1ractive <kevin.jn99@gmail.com>
2023-09-21 16:24:22 -0300

    resolve errors from home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+
 import Home from "./components/Home";
 
 function App() {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import LogoP5 from "../assets/p5.png";
 import InProgress from "../assets/5695859.png";
 import "./home.css";
 
@@ -10,9 +9,6 @@ const Home = () => {
         <div className="home-inprogress">
           <img alt="Home en progreso" src={InProgress} class="homeInProgress" />
         </div>
-        {/* <div>
-          <img alt="Logo de p5" src={LogoP5} class="homeLogoP5" />
-        </div> */}
         <div className="home-title">
           <h1 class="title">House Of Dev</h1>
         </div>
