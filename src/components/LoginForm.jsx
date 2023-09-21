c88ff67d45a37afd0468a20258f0a2a822c2a1d5
santimoran2720 <moran3490@gmail.com>
2023-09-21 18:19:12 -0300

    loginForm y estilos

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,92 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios
+      .post("/login", {
+        email: email,
+        password: password,
+      })
+      .then(() => {
+        console.log("Has iniciado sesión");
+        navigate("/");
+      })
+
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  
+
+  return (
+    
+    <div className="retroForm">
+      <form onSubmit={handleLogin}>
+        <h2 className="house">HOUSE OF DEV</h2>
+        <h2 className="achicarLetra">Tu nueva vivienda esta aqui.</h2>
+        <div className="mb-3" >
+          
+          <label  htmlFor="exampleInputEmail1" className="form-label">
+           
+          </label>
+          <input
+          placeholder="EMAIL"
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onChange={onChangeEmail}
+            required
+          />
+        </div>
+        <br />
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            
+          </label>
+          <input
+          placeholder="PASSWORD"
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            onChange={onChangePassword}
+            required
+          />
+        </div>
+        <br />
+        <Link to={"/home"}>
+          <button type="submit" className="form-control">
+            Iniciar sesión
+          </button>
+        </Link>
+      </form>
+      <br></br>
+      <p>No tienes una cuenta?</p>
+      <Link to={"/register"}>
+        <button type="submit" className="form-control">
+          Registrarse
+        </button>
+      </Link>
+      
+      
+     
+    </div>
+  );
+};
+
+export default LoginForm;
