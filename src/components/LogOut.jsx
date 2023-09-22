e9cdb35ff56d004f69786d77d70e8f0710f130b4
German Cuevas <54972367+GermanCuevas@users.noreply.github.com>
2023-09-22 13:10:20 -0300

    Update LogOut.jsx

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 function logOut() {
   const handleLogout = () => {
     axios.post("/api/users/logout").then(() => {
-      toast.success("Has cerrado sesión");
       setTimeout(() => {
         navigate("/");
         onSubmitReload();
